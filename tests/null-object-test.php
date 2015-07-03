0a3db31f3441f87516e4dd6c46a5cf5304d65d15
Chris Marlow <chris.marlow@achievers.com>
2015-07-02 22:59:33 -0400

    Adding a logger

--- tests/null-object-test.php
+++ tests/null-object-test.php
@@ -9,6 +9,8 @@ class NullObjectTest extends PHPUnit_Framework_TestCase
 
 	public function testthatNullObjectWorks()
 	{
+		logger( $this->subject->imaginaryProperty);
+
 		$this->assertInstanceOf( 'NullObject', $this->subject,
 			'Subject is a NullObject' );
 		$this->assertNotNull( $this->subject ,
@@ -16,4 +18,11 @@ class NullObjectTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull( $this->subject->imaginaryProperty ,
 			'NullObject properties are never null' );
 	}
+}
+
+
+function logger( $msg ) {
+	print_r( "\n" );
+	print_r( $msg );
+	print_r( "\n" );
 }
