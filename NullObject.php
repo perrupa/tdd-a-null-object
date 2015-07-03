ad896353563dd6b7bf9a2172d035c31b714570ba
Chris Marlow <chris.marlow@achievers.com>
2015-07-02 23:58:58 -0400

    Giving NullObject a toString method

--- NullObject.php
+++ NullObject.php
@@ -1,15 +1,26 @@
 <?php
 trait NullPattern {
+
 	function __get( $key )
 	{
 		return $this;
 	}
+
 	function __call( $key, $arguments )
 	{
 		return $this;
+	}
+
+	function __toString()
+	{
+		return "NullObject()";
 	}
 }
 
 class NullObject {
 	use NullPattern;
+
+	function __construct( $value = null )
+	{
+	}
 }
