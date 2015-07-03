1f2b14f15550332603c94a2e838bdfab222ee528
Chris Marlow <chris.marlow@achievers.com>
2015-07-02 22:35:26 -0400

    Basic tests almost running

--- tests/null-object-test.php
+++ tests/null-object-test.php
@@ -1,4 +1,17 @@
 <?php
-class StackTest extends PHPUnit_Framework_TestCase 
+class StackTest extends PHPUnit_Framework_TestCase
 {
+	function setUp() {
+		$this->subject = new NullObject();
+	}
+
+	public function testthatNullObjectWorks()
+	{
+		$this->assertInstanceOf( $this->subject, NullObject,
+			'Subject is a NullObject' );
+		$this->$this->assertNotNull( $this->subject ,
+			'NullObject ain\'t null' );
+		$this->$this->assertNotNull( $this->subject->imaginaryProperty ,
+			'NullObject properties are never null' );
+	}
 }
